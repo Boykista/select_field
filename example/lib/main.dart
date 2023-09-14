41a76da173518e80cf949e227e14ea128e31a9be
Boykista <dorian.bojic25@gmail.com>
2023-09-14 10:57:00 +0200

    title changed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Select field',
+      title: 'Select Field',
       theme: themeData,
       home: const MyHomePage(),
     );
